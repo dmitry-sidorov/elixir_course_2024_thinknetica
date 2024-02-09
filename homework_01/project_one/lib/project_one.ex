39a9e943c96329b85f6ee6b809b89cba27b87475
dzmitry_sidorov <jimmaggot@mail.ru>
2024-02-09 23:27:07 +0300

    HW01. Add chmod calculating

--- homework_01/project_one/lib/project_one.ex
+++ homework_01/project_one/lib/project_one.ex
@@ -4,15 +4,28 @@ defmodule ProjectOne do
   """
 
   @doc """
-  Hello world.
+  Hello chmod.
 
   ## Examples
 
-      iex> ProjectOne.hello()
-      :world
+      iex> ProjectOne.calculate_chmod(%{user: "rwx", group: "r-x", other: "r-x"})
+      "755"
 
   """
-  def hello do
-    :world
+  def calculate_chmod(perm) do
+    user = Map.get(perm, :user)
+    group = Map.get(perm, :group)
+    other = Map.get(perm, :other)
+    "#{calculate_permission(user)}#{calculate_permission(group)}#{calculate_permission(other)}"
+  end
+
+  defp calculate_permission(nil), do: "0"
+  defp calculate_permission(code) do
+    {num, _} = code
+    |> String.replace(~r/\w/, "1")
+    |> String.replace(~r/-/, "0")
+    |> Integer.parse(2)
+
+    num
   end
 end
--- homework_01/project_one/test/project_one_test.exs
+++ homework_01/project_one/test/project_one_test.exs
@@ -2,7 +2,25 @@ defmodule ProjectOneTest do
   use ExUnit.Case
   doctest ProjectOne
 
-  test "greets the world" do
-    assert ProjectOne.hello() == :world
+  test "calculates chmod" do
+    assert ProjectOne.calculate_chmod(%{user: "rwx", group: "r-x", other: "r-x"}) == "755"
+    assert ProjectOne.calculate_chmod(%{user: "rwx", group: "r--", other: "r--"}) == "744"
+    assert ProjectOne.calculate_chmod(%{user: "r-x", group: "r-x", other: "r-x"}) == "555"
+  end
+
+  test "calculates chmod with missing key" do
+    assert ProjectOne.calculate_chmod(%{user: "r-x", group: "rwx"}) == "570"
+    assert ProjectOne.calculate_chmod(%{group: "rwx"}) == "070"
+    assert ProjectOne.calculate_chmod(%{other: "rwx"}) == "007"
+  end
+
+  test "calculates chmod for empty map" do
+    assert ProjectOne.calculate_chmod(%{}) == "000"
+  end
+
+  test "raises exception for wrong argument" do
+    assert_raise BadMapError, fn ->
+      ProjectOne.calculate_chmod("not map value")
+    end
   end
 end
