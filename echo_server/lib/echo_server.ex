103e6f18b09e53abeab248338a281ae6a5cc36e9
dzmitry_sidorov <jimmaggot@mail.ru>
2024-02-20 02:03:43 +0300

    HW02. Add simple functionality

--- echo_server/lib/echo_server.ex
+++ echo_server/lib/echo_server.ex
@@ -1,4 +1,6 @@
 defmodule EchoServer do
+  use GenServer
+
   @moduledoc """
   Documentation for `EchoServer`.
   """
@@ -7,12 +9,35 @@ defmodule EchoServer do
   Hello world.
 
   ## Examples
-
-      iex> EchoServer.hello()
-      :world
+      iex> {:ok, pid} = EchoServer.start_link()
+      iex> EchoServer.echo(pid, :ping)
+      {:pong, :nonode@nohost}
 
   """
-  def hello do
-    :world
+
+  # Client
+
+  def start_link(default \\ %{}) do
+    GenServer.start_link(__MODULE__, default)
+  end
+
+  def echo(pid, message) do
+    case message do
+      :ping -> GenServer.call(pid, message)
+      _ -> raise "Only :ping message is allowed!"
+    end
+
+  end
+
+  # Server (callbacks)
+
+  @impl true
+  def init(state) do
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_call(:ping, _from, state) do
+    {:reply, {:pong, node()}, state}
   end
 end
--- echo_server/test/echo_server_test.exs
+++ echo_server/test/echo_server_test.exs
@@ -2,7 +2,13 @@ defmodule EchoServerTest do
   use ExUnit.Case
   doctest EchoServer
 
-  test "greets the world" do
-    assert EchoServer.hello() == :world
+  test "reply to :ping message" do
+    {:ok, pid} = EchoServer.start_link()
+    assert EchoServer.echo(pid, :ping) == {:pong, :nonode@nohost}
+  end
+
+  test "throw error" do
+    {:ok, pid} = EchoServer.start_link()
+    assert_raise RuntimeError,  "Only :ping message is allowed!", fn -> EchoServer.echo(pid, :some) == {:pong, :nonode@nohost} end
   end
 end
