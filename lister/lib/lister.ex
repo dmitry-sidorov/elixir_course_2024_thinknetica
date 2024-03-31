61f82810137fb5e9e73569ad4a5ca45df74b5b40
dzmitry_sidorov <jimmaggot@mail.ru>
2024-03-31 16:24:14 +0300

    HW07. Implement protocols for common data types

--- lister/lib/lister.ex
+++ lister/lib/lister.ex
@@ -3,16 +3,52 @@ defmodule Lister do
   Documentation for `Lister`.
   """
 
-  @doc """
-  Hello world.
+  defprotocol Listable do
+    @spec show(any()) :: any()
+    def show(term)
+  end
 
-  ## Examples
+  defimpl Listable, for: BitString do
+    @spec show(binary()) :: list()
+    def show(term) do
+      String.to_charlist(term)
+    end
+  end
 
-      iex> Lister.hello()
-      :world
+  defimpl Listable, for: List do
+    @spec show(list()) :: list()
+    def show(term) do
+      term
+    end
+  end
 
-  """
-  def hello do
-    :world
+  defimpl Listable, for: [Float, Integer] do
+    @spec show(integer()) :: [...]
+    def show(term) do
+      [term]
+    end
+  end
+
+  defimpl Listable, for: Map do
+    def show(term) do
+      term |> Map.to_list()
+    end
+  end
+
+  defimpl Listable, for: Tuple do
+    def show(term) do
+      term |> Tuple.to_list()
+    end
+  end
+
+  defimpl Listable, for: Atom do
+    @spec show(atom()) :: list()
+    def show(term) do
+      term |> Atom.to_string() |> String.to_charlist()
+    end
+  end
+
+  def show(term) do
+    Listable.show(term)
   end
 end
