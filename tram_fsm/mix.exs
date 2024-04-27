8a528cfe058038fbe9f127ea62559ce1ee2f2e8d
dzmitry_sidorov <jimmaggot@mail.ru>
2024-04-28 00:43:24 +0300

    HW09. Add dialixir and specs

--- tram_fsm/mix.exs
+++ tram_fsm/mix.exs
@@ -21,7 +21,8 @@ defmodule TramFsm.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:stream_data, "~> 0.5", only: :test}
+      {:stream_data, "~> 0.5", only: :test},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 end
